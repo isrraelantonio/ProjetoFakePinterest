240841965c1c95634852d694d70395f18e93ad38
isrraelantonio <israelantonio027@gmail.com>
2024-10-09 10:22:46 -0300

    relacionado rotas

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
 
 app =  Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
-
+app.config["SECRET_KEY"] = "f97c4e1ca25f6c386168e299b1ffa0a2"
 database = SQLAlchemy(app)
-
+bycript = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "homepage"
 
 from fakepinterest import routes
--- fakepinterest/forms.py
+++ fakepinterest/forms.py
@@ -1,1 +1,28 @@
 #formulários
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from fakepinterest.models import Usuario, Foto
+
+
+
+
+class FormLogin(FlaskForm):
+    email = StringField("Email",validators= [DataRequired(), Email()])
+    senha = PasswordField("Senha", validators= [DataRequired()])
+    botao = SubmitField("Fazer login")
+
+
+
+class FormCriarConta(FlaskForm):
+    email = StringField("Email", validators= [DataRequired(), Email()])
+    username = StringField("Nome de usuário", validators= [DataRequired()])
+    senha = PasswordField("Senha", validators= [DataRequired(), Length(6, 20)])
+    confirmacao_senha = PasswordField("confirme a senha", validators= [DataRequired(), EqualTo("senha")])
+    botao_confirmacao = SubmitField("confirmar")
+
+
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email = email.data).first()
+        if usuario:
+            return ValidationError("Email já cadastrado, faça login para continuar.")
